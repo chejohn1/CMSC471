ee52f5b8743f7f470276464e587bf5ca05acaa6d
amanno1 <amanno1@umbc.edu>
2014-04-14 00:15:17 -0400

    Create TheBeansOfWrath.lisp

---
+++ TheBeansOfWrath.lisp
@@ -0,0 +1,16 @@
+(defpackage :TheBeansOfWrath)
+(in-package :TheBeansOfWrath)
+
+(import '(user::BeanTypes 
+	  user::BeanConversion
+	  user::game-deck-stats user::game-discards user::game-discard-stats 
+	  user::game-coin-stats user::game-players user::game-rounds 
+	  user::game-shuffles
+	  user::player-name user::player-hand user::player-faceup
+	  user::player-numfields user::player-fields user::player-coins
+	  user::player-coin-stack
+	  user::plant user::harvest user::bean-fits
+	  user::legal-fields-to-harvest user::plant-card-at-front
+	  user::harvest-rate user::buy-third-bean-field
+	  user::is-singleton? user::is-multiple? user::is-empty?
+	  user::is-planted?))

8ed274686fa3bfc36cebd3672c1c9f7f5d3f92a9
Alex Manno <amanno1@umbc.edu>
2014-04-17 22:38:57 -0400

    Updated player's hand in optionally-plant-card

--- TheBeansOfWrath.lisp
+++ TheBeansOfWrath.lisp
@@ -28,19 +28,12 @@
 
 (defun optionally-plant-card (player game)
   (buy-third-bean-field player game)
-  (plant-card player (car (player-hand player)) game))
+  (plant-card player (pop (player-hand player)) game))
 
 (defun handle-face-up-cards (player game)
   (buy-third-bean-field player game)
   (plant-card player (pop (player-faceup player)) game)
   (plant-card player (pop (player-faceup player)) game))
-	
-(defun harvest-best-field (player game)
-  (let ((which (legal-fields-to-harvest (player-fields player))))
-    (cond ((not which)
-	   (error "No legal fields to harvest"))
-	  (t (harvest player (cdr which) game)))
-    (car which)))
 
 ;;; returns true if the player has three fields, null if only two
 (defun third-field? (player)
